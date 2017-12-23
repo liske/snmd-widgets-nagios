87e3d8370945d7d288c117bc5fc73474afd39484
Thomas Liske <thomas@fiasko-nw.net>
2017-12-23 12:29:21 +0100

    Add SNMD logo.

--- doc/conf.py
+++ doc/conf.py
@@ -2,3 +2,13 @@
 
 project = 'snmd-widgets-nagios'
 master_doc = 'index'
+copyright = u'2016 - 2017, Thomas Liske'
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo='_static/snmd-logo.svg'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/snmd-logo.png'
